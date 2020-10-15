00d5fc08c499872385224d0515d4485466b6b5d1
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2020-10-15 13:16:34 -0300

    list user fix

--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { Button } from 'antd';
 
 import api from '../services/api';
 
 import Layout from '../components/Layout';
 import User from '../components/User';
 
+import '../styles/pages/company_list.css';
+
 function UserList() {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const history = useHistory();
 
@@ -16,6 +20,7 @@ function UserList() {
       const {data} = await api.get('/users');
 
       setUsers(data);
+      setLoading(false);
     }
 
     getAllUsers();
@@ -40,6 +45,15 @@ function UserList() {
 
         {!users && (
           <span>Carregando...</span>
+        )}
+
+        {users.length === 0 && !loading && (
+          <>
+          <span>Nenhum usuário cadastrado</span>
+          <Button className="button_register" onClick={() => { history.push('/user_register') }}>
+            <span>Cadastrar usuário</span>
+          </Button>
+          </>
         )}
       </div>
     </div>
